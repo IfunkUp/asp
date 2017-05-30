be038eade0dc3e6679c1046ea1dc2a0358871f65
steve dekerf <steve.dekerf@hotmail.com>
2017-05-31 00:12:47 +0200

    w

---
+++ contactweb/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace contactweb.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhonePrimary { get; set; }
+        public string phoneSecondary { get; set; }
+        public DateTime Birthday { get; set; }
+        public string StreetAdress1 { get; set; }
+        public string StreetAdress2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+    }
+}
